a1fb775db6a52969803dcaccc5be54f3136a699a
tn23364 <164087727+tn23364@users.noreply.github.com>
2024-04-09 10:15:21 +1200

    find file type

---
+++ COMP_03_file_type.py
@@ -0,0 +1,46 @@
+# asks users for file type (integer / image / text / xxx)
+
+def get_filetype():
+
+    while True:
+        response = input('file type: ').lower()
+
+        # check for "i" or the exit code
+        if response == "xxx" or response == "i":
+            return response
+
+        # check if it's an integer...
+        elif response in ['integer', 'int']:
+            return "integer"
+
+        # check for an image...
+        elif response in ['image', 'picture', 'img', 'p']:
+            return 'image'
+        
+        # check for text...
+        elif response in ['text', 'txt', 't']:
+            return 'text'
+
+        # if the response is invalid output an error
+        else:
+            print('please enter a valid file type')
+
+
+# main routine goes here
+while True:
+    file_type = get_filetype()
+
+    # if user chose 'i', ask if they want an image or an integer
+    if file_type == 'i':
+
+        want_image = input('Press <enter> for an integer or any key for an image. ')
+        if want_image == "":
+            file_type = 'integer'
+        else:
+            file_type = 'image'
+
+
+    print(f'You chose {file_type}')
+
+    if file_type == 'xxx':
+        break
